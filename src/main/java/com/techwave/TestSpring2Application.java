38fa808e83302f64f65ede4db42883f3b8d8ba66
Raju Kumar <rajukaumarsahu.29@gmail.com>
2023-07-18 16:26:04 +0530

    new project

---
+++ src/main/java/com/techwave/TestSpring2Application.java
@@ -0,0 +1,13 @@
+package com.techwave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpring2Application.class, args);
+	}
+
+}
